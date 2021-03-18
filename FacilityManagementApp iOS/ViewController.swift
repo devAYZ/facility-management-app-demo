b61b02dea50a9b8665d3a9f20445a464eb39ef91
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-03-18 17:03:22 +0100

    Initial Commit

---
+++ FacilityManagementApp iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FacilityManagementApp iOS
+//
+//  Created by waheedCodes on 18/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
