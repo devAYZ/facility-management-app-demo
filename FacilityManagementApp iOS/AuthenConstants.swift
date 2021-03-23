0a0d5ab218537f5cfd813b151084de290daf4b7d
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-03-24 00:15:59 +0100

    added auth constants

--- FacilityManagementApp iOS/AuthenConstants.swift
+++ FacilityManagementApp iOS/AuthenConstants.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+struct AuthenConstants {
+    static let clientSecret = "xuNK_90u9Xj.kkchZKLzS6xvW1yV.C2s.0"
+    static let authorityError = "There was an error getting the authority URL...Try again"
+    static let initError = "There was an error creating MSAL Public client"
+    static let userAccountError = "There was an error getting the user account"
+    static let silentTokenError = "There was an error getting the token silently"
+    static let interactiveTokenError = "There was an error getting the token interactively"
+    static let signOutError = "There was an error signing you out"
+    static let interactiveTokenSuccess = "The token was successfully gotten interactively"
+    static let silentTokenSuccess = "The token was successfully gotten silently"
+    static let authoritySuccess = "The authority URL exists"
+    static let silentToken = "Account yet to be inputed in cache"
+}
